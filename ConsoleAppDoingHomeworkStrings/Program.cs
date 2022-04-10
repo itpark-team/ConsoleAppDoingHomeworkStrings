74324e4ceaff7db860c3703957e1e9dda4fb6233
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-04-10 11:18:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppDoingHomeworkStrings/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDoingHomeworkStrings
+{
+    internal class Program
+    {
+        static bool isNumber(char symb)
+        {
+            return symb >= '0' && symb <= '9';
+        }
+        static void Main(string[] args)
+        {
+            string inputStr;
+            int totalSum = 0;
+
+            int currentNumberInInt = 0;
+            string currentNumberInString = "";
+
+            Console.Write("введите строку с числами внутри: ");
+            inputStr = Console.ReadLine();
+
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                if(isNumber(inputStr[i])==true)
+                {
+                    currentNumberInString += inputStr[i];
+                    i++;
+
+                    while(i<inputStr.Length && isNumber(inputStr[i]) == true)
+                    {
+                        currentNumberInString += inputStr[i];
+                        i++;
+                    }
+
+                    currentNumberInInt = int.Parse(currentNumberInString);
+
+                    totalSum += currentNumberInInt;
+
+                    currentNumberInString = "";
+                }
+            }
+
+            Console.WriteLine("сумма чисел в строке  = " + totalSum);
+
+            Console.ReadKey();
+        }
+    }
+}
